207cfab44e8619cc412c70ab991ee9f9fa9c46f2
Michael-C-Kirk <michael_c_kirk@yahoo.com>
2018-08-17 23:25:56 -0700

    Updated player.h file

--- Game/player.h
+++ Game/player.h
@@ -1,24 +1,35 @@
 using namespace std;
 
-class Player
+class Player()
 {
 public:
 	// variables
 	int health = 100;
 	int maxHealth = 100;
 	int armor = 0;
+	int maxArmor = 50
 	string name = "";
 	int age = 0;
 	int sex = 0;
+	int xp = 0;
+	
 
 	// member functions
-	Player(){}
+	Player()
+	{}
 
-	void printInfo()
+	void print_info()
 	{
 		cout << "Name: " << name << endl;
 		cout << "Age: " << age << endl;
 		cout << "Sex: " << sex << endl;
 		cout << "Health: " << health << endl;
 	}
+
+	bool player_dead()
+	{
+		return health == 0;
+	}
+
+
 };
