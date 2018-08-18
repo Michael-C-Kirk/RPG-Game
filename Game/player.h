5601f6de14941e60e3b4ae6c329009364e4a070a
Michael-C-Kirk <michael_c_kirk@yahoo.com>
2018-08-17 22:25:38 -0700

    Made player.h file

---
+++ Game/player.h
@@ -0,0 +1,24 @@
+using namespace std;
+
+class Player()
+{
+public:
+	// variables
+	int health = 100;
+	int maxHealth = 100;
+	int armor = 0;
+	string name = "";
+	int age = 0;
+	int sex = 0;
+
+	// member functions
+	Player::Player(){}
+
+	void printInfo()
+	{
+		cout << "Name: " << name << endl;
+		cout << "Age: " << age << endl;
+		cout << "Sex: " << sex << endl;
+		cout << "Health: " << health << endl;
+	}
+};
